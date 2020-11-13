b6b8881de26e7dc0fafe5bda9af528f7934aa1e5
Antoine07 <antoine.lucsko@gmail.com>
2020-11-13 12:50:56 +0100

    cra

---
+++ les_fondamentaux/part4_squelette_router/cra/denomination/src/App.js
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+
+import Denomination from './components/Denomination';
+
+import './App.css';
+
+class App extends  Component{
+
+  render(){
+
+    return (
+      <p>Hello React</p>
+    )
+  }
+}
+
+export default App;
---
+++ les_fondamentaux/part4_squelette_router/cra/denomination/src/components/Denomination.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+
+class Denomination extends  Component{
+
+  render(){
+
+    return (
+      <p>Hello Denomination</p>
+    )
+  }
+}
+
+export default Denomination;
